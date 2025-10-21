67c67636b2d4dd199775dbd65447be1e8b1784cb
david-bol12 <davidboland1001@gmail.com>
2025-10-21 16:16:29 +0100

    test
    test

---
+++ project/diskInfo.java
@@ -0,0 +1,17 @@
+/*
+ *  Disk information class for JNI
+ *
+ *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
+ */
+
+public class diskInfo 
+{
+    // Refresh the current values and counters - call this before other methods
+    public native void read ();
+    public native int diskCount ();
+    public native String getName (int disk);
+    public native long getTotal (int disk);
+    public native long getUsed (int disk);
+    public native long getAvailable (int disk);
+}
+
---
+++ project/sysInfo.java
@@ -0,0 +1,11 @@
+/*
+ *  Sample system information class for JNI
+ *
+ *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
+ */
+
+public class sysInfo 
+{
+     public native int intExample(int n);
+     public native String stringExample(String s);
+}
