edc656392175f2a017238f4160e004814e9f7f61
Ciallian <ciallianbain@gmail.com>
2025-10-23 11:34:31 +0100

    See if in branch

--- project/Main.java
+++ project/Main.java
@@ -12,7 +12,7 @@ public class Main
     {
         System.out.println("System Info Project");
         System.loadLibrary("sysinfo"); // Loads C++ Library
-
+        System.out.println("in Disk branch");
     }
 }
 
