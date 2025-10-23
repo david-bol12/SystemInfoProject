f7ef2b6d25dab276b52b154b447b59f6b75171da
Ciallian <ciallianbain@gmail.com>
2025-10-23 12:08:17 +0100

    mem read

--- project/Main.java
+++ project/Main.java
@@ -4,15 +4,22 @@
  *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
  */
 
+
+
 public class Main
 {
 
     // Create a new branch before editing any code in main
     public static void main(String[] args)
     {
+       
         System.out.println("System Info Project");
         System.loadLibrary("sysinfo"); // Loads C++ Library
         System.out.println("in Disk branch");
+        memInfo memory = new memInfo();
+        memory.read();
+        System.out.println(memory.getTotal());
+        
+        
     }
 }
-
