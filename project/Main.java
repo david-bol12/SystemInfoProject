c95f1e443ef026cf2b0d3cc437feb31067567901
Padraig Meade <padraig.meade.bally66@gmail.com>
2025-10-24 12:51:19 +0100

    hope

--- project/Main.java
+++ project/Main.java
@@ -3,19 +3,5 @@
  *
  *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
  */
-public class Main {
-    public static void main(String[] args) throws InterruptedException
-    {
+public class Main {}
 
-          System.out.println("System Info Project");
-        System.loadLibrary("sysinfo"); // Loads C++ Library
-        System.out.println("In bus branch");
-
-        
-        usbInfo u1 = new usbInfo();
-        u1.read();; 
-        DevInfo d1 = new DevInfo();
-        d1.devInfo();
-    }
-
-}
