a2a7ad6576227d2e52fb1fb2609e3a7dc672c5e9
Padraig Meade <padraig.meade.bally66@gmail.com>
2025-10-24 11:42:07 +0100

    work pleaee

--- project/Main.java
+++ project/Main.java
@@ -6,6 +6,12 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException
     {
+
+          System.out.println("System Info Project");
+        System.loadLibrary("sysinfo"); // Loads C++ Library
+        System.out.println("In bus branch");
+
+        
         usbInfo u1 = new usbInfo();
         u1.read();; 
         DevInfo d1 = new DevInfo();
