7e4f10fe0e59078ebbf4fc240ac3c7b7525fdddb
Padraig Meade <padraig.meade.bally66@gmail.com>
2025-10-24 12:59:36 +0100

    Blank Slate, lets get this to work

--- project/Main.java
+++ project/Main.java
@@ -3,5 +3,14 @@
  *
  *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
  */
-public class Main {}
+public class Main {
+ public static void main(String[] args) {
+    
+         
+    
+        System.out.println("System Info Project");
+        System.loadLibrary("sysinfo"); // Loads C++ Library
+        System.out.println("In bus branch");
+ }
+}
 
