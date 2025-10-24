d6de77e81969a611166733d3140de207b6646740
Ciallian <ciallianbain@gmail.com>
2025-10-24 11:16:35 +0100

    Memory and disk test

--- project/Main.java
+++ project/Main.java
@@ -15,11 +15,58 @@ public class Main
        
         System.out.println("System Info Project");
         System.loadLibrary("sysinfo"); // Loads C++ Library
-        System.out.println("in Disk branch");
-        memInfo memory = new memInfo();
-        memory.read();
-        System.out.println(memory.getTotal());
+        showMemInfo m1 = new showMemInfo();
+        m1.showMemoryUsage();
+        showDiskInfo d1 = new showDiskInfo();
+        d1.diskMethod();
         
+}
         
     }
+class showMemInfo {
+            private memInfo memory;
+
+            public showMemInfo() {
+                memory = new memInfo();
+            }
+
+        public void showMemoryUsage() {
+        memory.read(); 
+
+        int total = memory.getTotal(); 
+        int used = memory.getUsed();
+
+        double totalGB = total / (1024.0 * 1024.0 * 1024.0);
+        double usedGB = used / (1024.0 * 1024.0 * 1024.0);
+        double freeGB = totalGB - usedGB;
+        double usedPercent = (usedGB / totalGB) * 100;
+
+        System.out.println(" Memory Information ");
+        System.out.printf("Total: %.2f GB%n", totalGB);
+        System.out.printf("Used:  %.2f GB (%.1f%%)%n", usedGB, usedPercent);
+        System.out.printf("Free:  %.2f GB%n", freeGB);
+    }
+}
+
+class showDiskInfo {
+    private diskInfo disk;
+
+    public showDiskInfo() {
+        disk = new diskInfo();
+    }
+
+    public void diskMethod() {
+        disk.read();
+
+        
+        for (int i = 0; i < disk.diskCount(); i++) {
+            double totalGB = disk.getTotal(i) / (1024.0 * 1024.0 * 1024.0);
+            System.out.println ("disk "+disk.getName(i)+" has "+
+                    disk.getTotal(i)+" blocks, of which "+
+                    disk.getUsed(i)+" are used");
+            System.out.printf("Block GB: %.2f%n", totalGB);
+        }
+
+    }
+            
 }
