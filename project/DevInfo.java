37f96313f70eb9e6c11558f00ab1749d07419d68
Padraig Meade <padraig.meade.bally66@gmail.com>
2025-10-24 12:36:37 +0100

    please just work

--- project/DevInfo.java
+++ project/DevInfo.java
@@ -1,63 +1,53 @@
-
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 public class DevInfo {
 
-    public native void read(); // Refreshes all current counters
+    private static final int SCAN_INTERVAL_MS = 2000; // 2 seconds
 
-     // 2000 ms interval between each while statement,esesntially creates an endless loop while running
+    public  void devInfo() throws InterruptedException {
+        usbInfo usb = new usbInfo(); // create usbInfo instance
+        Set<String> previousDevices = new HashSet<>();
 
+        System.out.println("Starting USB monitoring...");
 
+        while (true) {
+            usb.read(); // refresh USB info
 
-         public void devInfo() throws InterruptedException { // Create a method called devInfo() which will raise an InterruptedException when certain conditions are met
-        usbInfo usb = new usbInfo(); // Create an instance of the usb class, this should help when reafding the usb bus
-        Set<String> previousDevices = new HashSet<>(); // Creates a hash set of the previous devices, needed to register what devices already exist in the bus
+            Set<String> currentDevices = new HashSet<>();
 
-while (true) {
-    // Refresh USB info
-usb.read();
+            int buses = usb.busCount();
+            for (int i = 1; i <= buses; i++) {
+                int deviceCount = usb.deviceCount(i);
+                System.out.println("Bus " + i + " has " + deviceCount + " devices");
 
-// Build current devices set
-Set<String> currentDevices = new HashSet<>();
-int buses = usb.busCount();
-for (int i = 1; i <= buses; i++) {
-    int deviceCount = usb.deviceCount(i);
-    for (int j = 1; j <= deviceCount; j++) {
-        int vendor = usb.vendorID(i, j);
-        int product = usb.productID(i, j);
+                for (int j = 1; j <= deviceCount; j++) {
+                    int vendor = usb.vendorID(i, j);
+                    int product = usb.productID(i, j);
 
-        // Skip dead devices (vendor/product 0x0000)
-        if (vendor == 0x0000 && product == 0x0000) continue;
+                    // Skip dead/placeholder devices
+                    if (vendor == 0x0000 && product == 0x0000) continue;
 
-        String deviceId = String.format("Bus %d | Vendor 0x%04X | Product 0x%04X", i, vendor, product);
-        currentDevices.add(deviceId);
-    }
-}
+                    String deviceId = String.format("Bus %d | Vendor 0x%04X | Product 0x%04X", i, vendor, product);
+                    currentDevices.add(deviceId);
 
-// Remove dead devices from previousDevices
-previousDevices.removeIf(d -> d.contains("0x0000"));
+                    // Detect inserted devices
+                    if (!previousDevices.contains(deviceId)) {
+                        System.out.println("🔌 USB device inserted: " + deviceId);
+                    }
+                }
+            }
 
-// Detect inserted devices
-for (String device : currentDevices) {
-    if (!previousDevices.contains(device)) {
-        System.out.println("USB device inserted: " + device);
-    }
-}
+            // Removal detection is NOT possible due to library limitations
+            // Any unplugged device will remain in the library's internal list
+            // Explain this in your report if required
 
-// Detect removed devices
-for (String device : previousDevices) {
-    if (!currentDevices.contains(device)) {
-        System.out.println("USB device removed: " + device);
-    }
-}
-
-// Update previousDevices for the next iteration
-previousDevices = new HashSet<>(currentDevices);
-
+            previousDevices = new HashSet<>(currentDevices); // copy for next iteration
+            Thread.sleep(SCAN_INTERVAL_MS);
         }
     }
 }
+
 
 /*  public static void main(String[] args) throws InterruptedException{
     usbInfo u1 = new usbInfo(); //calls upon the usbInfo class which will refresh the all
