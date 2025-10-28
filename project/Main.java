84496cd728655bdcceb900637fff37e365f5d0dd
Ciallian <ciallianbain@gmail.com>
2025-10-28 16:07:58 +0000

    psuhhhh

--- project/Main.java
+++ project/Main.java
@@ -8,44 +8,11 @@
 
 public class Main
 {
-public static void showDisk()
-    {
-        Disk[] disks = Disk.getDisks();
-        for (Disk d : disks) {
-            System.out.printf("Disk: %s, Type: %s, Total Memory: %.2f GiB (%.2f GB), Used Memory: %.2f GiB (%.2f GB) (%.1f%%), Total Free: %.2f GiB (%.2f GB) (%.1f%%) Current Health: %s%n",
-                d.getName(),
-                d.getType(),
-                d.getTotalGiB(),
-                d.getTotalGB(),
-                d.getUsedGiB(),
-                d.getUsedGB(),
-                d.getPercentUsed(),
-                d.getFreeGiB(),
-                d.getFreeGB(),
-                d.getPercentFree(),
-                d.getHealthStatus()
-                );
-        }
-    }
-    
-    public static void showMem()
-  {
-        memInfo mem = new memInfo();
-        mem.read();
-
-        System.out.println(" Memory Information ");
-        System.out.printf("Total: %.2f GiB or %.2f GB%n", mem.getTotalGiB(), mem.getTotalGB());
-        System.out.printf("Used:  %.2f GiB or %.2f GB (%.1f%%)%n", mem.getUsedGiB(), mem.getUsedGB(), mem.getPercentUsed());
-        System.out.printf("Free:  %.2f GiB or %.2f GB (%.1f%%)%n", mem.getFreeGiB(), mem.getFreeGB(), mem.getPercentFree());
-        System.out.printf("Status: %s%n", mem.getMemoryStatus());
-    }
-    // Create a new branch before editing any code in main
     public static void main(String[] args)
     {
         System.out.println("System Info Project");
         System.loadLibrary("sysinfo"); // Loads C++ Library
-        showDisk();
-        showMem();
+    
     }
 }
 
