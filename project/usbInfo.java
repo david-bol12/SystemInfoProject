a1126aed220a5a26e06b79e80e77a15694a71cbe
Padraig Meade <padraig.meade.bally66@gmail.com>
2025-10-28 16:21:12 +0000

    Finally fixed reading the usb

--- project/usbInfo.java
+++ project/usbInfo.java
@@ -5,6 +5,7 @@
  */
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -26,7 +27,65 @@ public class usbInfo
     // Return the product ID of a USB device
     public native int productID (int bus, int device);
 
-    static Set<Map.Entry<Integer, Integer>> pairs = new HashSet<>();
+    static Set<Map.Entry<Integer, Integer>> pairs = new HashSet<>();public Set<Map.Entry<Integer, Integer>> getVendorIDs() {
+    Set<Map.Entry<Integer, Integer>> pairs = new HashSet<>();
+    for (int i = 1; i <= busCount(); i++) {
+        // Iterate through all devices on the bus
+            for (int j = 1; j <= deviceCount(i); j++) {
+                int vendor = vendorID(i, j);
+                int product = productID(i, j);
+
+                if (vendor != 0 && product != 0) {	//replaces key and value with vendor and product
+                    pairs.add(new AbstractMap.SimpleEntry<>(vendor, product));            
+    }
+}
+}
+return pairs;
+/*static ArrayList<Integer>  getVen()
+{ ArrayList<Integer> ven = new ArrayList<Integer>();
+ ven.add(vendor);
+ return ven;
+} */
+
+
+/*       System.out.println("Bus " + i + " device " + validDeviceCount +
+                        " has vendor " + String.format("0x%04X", vendor) +
+                        " and product " + String.format("0x%04X", product)); */
+}
+
+public ArrayList getDevices() {
+        Set<Map.Entry<Integer, Integer>> pairs = getVendorIDs();
+        ArrayList <String> devices = new ArrayList<String>();
+            for(int i = 1; i <= busCount(); i++) {
+                int validDeviceCount = 0;
+
+                     for (int j = 1; j <= deviceCount(i); j++) {
+                         int vendor = vendorID(i ,j);
+                         int product = productID(i, j);
+
+                            if (vendor != 0 && product != 0) {
+                                validDeviceCount++;
+                                devices.add("Bus " + i + " device " + validDeviceCount +
+                                    " has vendor " + String.format("0x%04X", vendor) +
+                                    " and product " + String.format("0x%04X", product));
+                            }    
+
+                              /*    System.out.println("Bus " + i + " device " + validDeviceCount +
+                                    " has vendor " + String.format("0x%04X", vendor) +
+                                    " and product " + String.format("0x%04X", product)); */
+
+
+
+                            
+                    }       
+
+
+
+            }
+
+    return devices;
+    }
+
 
     public static void showUSB()
     {
