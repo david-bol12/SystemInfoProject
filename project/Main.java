fef5cc62d923b7225b23ef19408925b938434111
Ciallian <ciallianbain@gmail.com>
2025-10-28 10:26:03 +0000

    update

--- project/Main.java
+++ project/Main.java
@@ -25,7 +25,7 @@ public static void showDisk()
         }
     }
     public static void showMem()
-    {
+ {
         memInfo mem = new memInfo();
         mem.read();
 
@@ -34,44 +34,17 @@ public static void showDisk()
         System.out.printf("Used:  %.2f GiB or %.2f GB (%.1f%%)%n", mem.getUsedGiB(), mem.getUsedGB(), mem.getPercentUsed());
         System.out.printf("Free:  %.2f GiB or %.2f GB (%.1f%%)%n", mem.getFreeGiB(), mem.getFreeGB(), mem.getPercentFree());
         System.out.printf("Status: %s%n", mem.getMemoryStatus());
-
     }
     // Create a new branch before editing any code in main
     public static void main(String[] args)
     {
-       
         System.out.println("System Info Project");
         System.loadLibrary("sysinfo"); // Loads C++ Library
-        //showMemInfo m1 = new showMemInfo();
-       // m1.showMemoryUsage();
         showDisk();
         showMem();
     }
 }
-/*class showMemInfo {
-            private memInfo memory;
 
-            public showMemInfo() {
-                memory = new memInfo();
-            }
-
-        public void showMemoryUsage() {
-        memory.read(); 
-
-        int total = memory.getTotal(); 
-        int used = memory.getUsed();
-
-        double totalGB = total / (1000.0 * 1000.0);
-        double usedGB = used / (1000.0 * 1000.0);
-        double freeGB = totalGB - usedGB;
-        double usedPercent = (usedGB / totalGB) * 100; //test
-
-        System.out.println(" Memory Information ");
-        System.out.printf("Total: %.2f GiB%n", totalGB);
-        System.out.printf("Used:  %.2f GiB (%.1f%%)%n", usedGB, usedPercent);
-        System.out.printf("Free:  %.2f GiB%n", freeGB);
-    }
-}*/
 
 
 
