b12977f6504f65c8bf754343e83a0f39ef451ffc
Fergus <1fergusoflynn@gmail.com>
2025-10-28 16:20:02 +0000

    test

--- project/pciInfo.java
+++ project/pciInfo.java
@@ -77,7 +77,12 @@ public class pciInfo
                                     " and product "+String.format("0x%04X", pci.productID(i,j,k)));
 
                             busDeviceFunction.add(pci.vendorID(i,j,k));
-                            System.out.println(vendorIDs.get(pci.vendorID(i,j,k)));
+                            if (vendorIDs.containsKey(pci.vendorID(i,j,k))) {
+                                System.out.println(vendorIDs.get(pci.vendorID(i, j, k)) + " is the vendor for this bus");
+                            }
+                            else{
+                                System.out.println("Vendor is unknown");
+                            }
                             busDeviceFunction.add(pci.productID(i,j,k));
                             System.out.println(busDeviceFunction);
 
