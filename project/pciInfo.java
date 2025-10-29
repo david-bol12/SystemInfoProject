55f2641d151426c0bfa2334ea996b4f588cec573
Fergus <1fergusoflynn@gmail.com>
2025-10-29 20:56:02 +0000

    Adding The json code to test

--- project/pciInfo.java
+++ project/pciInfo.java
@@ -83,7 +83,7 @@ public class pciInfo {
                                     //System.out.println(vendorMap.get("0x0808"));
 
                                 String vendor = String.format("0x%04X", pci.vendorID(i, j, k));
-                                String product = String.format("0x%04X", pci.vendorID(i, j, k));
+                                String product = String.format("0x%04X", pci.productID(i, j, k));
                                 System.out.println(vendor);
                                 System.out.println(product);
                                 HashMap<String, HashMap<String, HashMap<String, String>>> bigMap = gson.fromJson(reader, type);
